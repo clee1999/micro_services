b034eb80fbd2918829dacc00cfee20c86044e0c5
Céline LY <lycelinepro99@gmail.com>
2021-12-14 10:56:51 +0100

    Add NavBar and Layout Css

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,60 +1,86 @@
+<style scoped>
+p {
+    font-family: "Nunito-SemiBold" !important;
+}
+
+a {
+}
+
+.itemList {
+    color: black !important;
+}
+</style>
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <v-app-bar
+            :color="bg"
+            fixed
+            elevate-on-scroll
+            scroll-target="#scrolling-techniques-7"
+        >
+            <v-toolbar-title white>L O G O</v-toolbar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+            <v-spacer></v-spacer>
+            <div class="d-inline-block pl-6"><p>Chercher un médecin</p></div>
+            <div class="d-inline-block pl-6"><p>Besoin d'aide ?</p></div>
+            <v-menu offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                    <p class="ml-6">
+                        <a dark v-bind="attrs" v-on="on">
+                            Se connecter
+                            <v-icon color="black">mdi-account-circle</v-icon>
+                        </a>
+                    </p>
+                </template>
+                <v-list>
+                    <v-list-item v-for="(item, index) in items" :key="index">
+                        <v-list-item-title
+                            ><p class="itemList">
+                                {{ item.title }}
+                            </p></v-list-item-title
+                        >
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-app-bar>
+        <v-container-fluid>
+            <v-main> </v-main>
+        </v-container-fluid>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
+    name: "App",
 
-  components: {
-    HelloWorld,
-  },
+    components: {},
 
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        bg: "transparent",
+        items: [
+            { title: "Click Me" },
+            { title: "Click Me" },
+            { title: "Click Me" },
+            { title: "Click Me 2" },
+        ],
+    }),
+    mounted() {
+        window.onscroll = () => {
+            console.log("scrooooolll");
+            this.changeColor();
+        };
+    },
+    methods: {
+        changeColor() {
+            if (
+                document.body.scrollTop > 100 ||
+                document.documentElement.scrollTop > 100
+            ) {
+                this.bg = "#1A0A61";
+            } else {
+                this.bg = "#1A0A61";
+            }
+        },
+    },
 };
 </script>
--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "./assets/_global.scss";
+// import VueRouter from "vue-router";
+// import Router from "./routes";
+// Vue.use(VueRouter);
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+// const router = new VueRouter({ routes: Router });
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+  // router: router,
+}).$mount("#app");
