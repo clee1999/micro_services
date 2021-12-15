9211eaf85acf561c193de9caccbc83687b3d6c98
Céline LY <lycelinepro99@gmail.com>
2021-12-15 17:09:23 +0100

    Ajout Dashboard Admin

---
+++ app/src/components/Admin/Dashboard.vue
@@ -0,0 +1,66 @@
+<style scoped lang="scss"></style>
+<template>
+    <div>
+        <v-container style="height: 100vh">
+            <v-row>
+                <v-col cols="12">
+                    <h1>Bienvenue {{ admin.name }} administrateur</h1>
+                    <h2>Votre dashboard</h2>
+                    <h3 class="mt-5">Voici la liste des médecins :</h3>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col cols="12">
+                    <div>
+                        <v-data-table
+                            :headers="headers"
+                            :items="doctors"
+                            item-key="name"
+                            class="elevation-1"
+                            :search="search"
+                            :custom-filter="filterOnlyCapsText"
+                        >
+                            <template v-slot:body.append>
+                                <tr>
+                                    <td colspan="4"></td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Dashboard",
+    data: () => ({
+        justify: ["center"],
+        admin: {
+            name: "Dr Ariel COHEN",
+            fonction: "Ophtalmologue",
+            adresse: "16 Avenue Anatole France 93600 Aulnay-sous-Bois",
+        },
+        search: "",
+        doctors: [
+            {
+                name: "Dr Ariel COHEN",
+                fonction: "Ophtalmologue",
+                adresse: "16 Avenue Anatole France 93600 Aulnay-sous-Bois",
+            },
+        ],
+    }),
+    computed: {
+        headers() {
+            return [
+                { text: "Name", value: "name" },
+                { text: "fonction", value: "fonction" },
+                { text: "adresse", value: "adresse" },
+            ];
+        },
+    },
+};
+</script>
--- app/src/routes.js
+++ app/src/routes.js
@@ -1,9 +1,11 @@
 import HomePage from "./components/HomePage.vue";
 import SearchDoctor from "./components/SearchDoctor.vue";
 import DoctorReservation from "./components/DoctorReservation.vue";
+import Dashboard from "./components/Admin/Dashboard.vue";
 
 export default [
   { path: "/", component: HomePage },
   { path: "/recherche", component: SearchDoctor },
   { path: "/docteur", component: DoctorReservation },
+  { path: "/admin", component: Dashboard },
 ];
